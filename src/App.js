9942891c1800185ef31abaf8038b08121f3fba91
KatiRasanen <kati.rasanen@teliacompany.com>
2021-10-11 21:08:02 +0300

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import img from "./profile_pic.jpg";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "UA-74408969-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
